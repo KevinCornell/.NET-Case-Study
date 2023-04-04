96a653737473de23929d9b22c46a671ecd39eb37
KevinCornell <kevdcornell@gmail.com>
2023-04-04 16:13:29 -0400

    AppComponent Tests Done

--- FrontEnd/src/app/components/card-creator/card-creator.component.spec.ts
+++ FrontEnd/src/app/components/card-creator/card-creator.component.spec.ts
@@ -1,17 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { CardCreatorComponent } from './card-creator.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('CardCreatorComponent', () => {
   let component: CardCreatorComponent;
   let fixture: ComponentFixture<CardCreatorComponent>;
+  const dialogRefMock = {
+    afterClosed() {
+      return of(true);
+    },
+    updateSize(widths?:string, height?:string){}
+  };
+  const dialogMock = {
+    open:() => dialogRefMock
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MatDialogModule],
       providers: [
-        { provide: MatDialogRef, useValue: {} },
+        { provide: MatDialog, useValue: dialogMock},
+        { provide: MatDialogRef, useValue: dialogRefMock },
         { provide: MAT_DIALOG_DATA, useValue: {}}
       ],
       declarations: [ CardCreatorComponent ],
--- FrontEnd/src/app/components/card-editor/card-editor.component.spec.ts
+++ FrontEnd/src/app/components/card-editor/card-editor.component.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
 import { CardEditorComponent } from './card-editor.component';
 
 describe('CardEditorComponent', () => {
   let component: CardEditorComponent;
   let fixture: ComponentFixture<CardEditorComponent>;
+  const dialogRefMock = {
+    afterClosed() {
+      return of(true);
+    },
+    updateSize(widths?:string, height?:string){}
+  };
+  const dialogMock = {
+    open:() => dialogRefMock
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardEditorComponent ]
+      imports:[MatDialogModule],
+      providers: [
+        { provide: MatDialog, useValue: dialogMock},
+        { provide: MatDialogRef, useValue: dialogRefMock },
+        { provide: MAT_DIALOG_DATA, useValue: {}}
+      ],
+      declarations: [ CardEditorComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
 
